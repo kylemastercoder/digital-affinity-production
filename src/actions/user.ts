039157d9786914c4f00e167446460413304ffde7
Kyle Andre David Lim <kyleandrelim17@gmail.com>
2024-08-15 09:51:15 +0800

    fixed env actions

--- src/actions/user.ts
+++ src/actions/user.ts
@@ -12,7 +12,7 @@ export const createUser = async (
 ) => {
   try {
     const response = await axios.post(
-      "https://digitalaffinity.kinde.com/api/v1/user",
+      `${process.env.KINDE_ISSUER_URL}/api/v1/user`,
       {
         profile: {
           given_name: givenName,
@@ -32,7 +32,7 @@ export const createUser = async (
         headers: {
           "Content-Type": "application/json",
           Accept: "application/json",
-          Authorization: `Bearer eyJhbGciOiJSUzI1NiIsImtpZCI6IjliOjQ0OjNlOmNhOjA2OjM2OjNjOjZkOmVhOmUyOjczOjAxOjYzOjBiOmNiOjk1IiwidHlwIjoiSldUIn0.eyJhdWQiOlsiaHR0cHM6Ly9kaWdpdGFsYWZmaW5pdHkua2luZGUuY29tL2FwaSJdLCJhenAiOiIyOGE4NDJiYjIxNjA0MWM1YWNhNWQyZDhhMmYxMjk1NyIsImV4cCI6MTcyMzcxNjM2MywiZ3R5IjpbImNsaWVudF9jcmVkZW50aWFscyJdLCJpYXQiOjE3MjM2Mjk5NjMsImlzcyI6Imh0dHBzOi8vZGlnaXRhbGFmZmluaXR5LmtpbmRlLmNvbSIsImp0aSI6ImM5ZjQyZmE5LThmODktNGI0Zi1hZGU0LWMyN2NmZGMxM2E2NiIsInNjb3BlIjoiIiwic2NwIjpbXX0.vQo8GSlm_rX7VpStLPIb5N4azBeDZBArrXrPuAbo804BxAtymcXVyabOEUxMMArMgUj3I7LnLUhyKSYBuhPvSai2akElDALG4AUjKhrmxBVHL_jsLskBGrO8sxMyLI1q4oPTBQBAt_VgTwtVFDOz6-BI7qBpWtJwHT0arP_6HVqfb9POfZ4sjQKmApNMSUx-WV4FDiLgI5AzAnZc6eqYejuSrK7suSQsfVDsyMAl874fKj9pUcBat-Xyzdx3JnU6gR_SHcNnIJq--52t3w9LRl08US2p31lyUTcqgSTy2Xud2dFLMefinWDVoqJixqfIWbwzSn539p7849QljXFSfw`,
+          Authorization: `Bearer ${process.env.KINDE_ACCESS_TOKEN}`,
         },
       }
     );
